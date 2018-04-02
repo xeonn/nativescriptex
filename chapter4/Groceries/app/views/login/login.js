b114a59398086b8eb67c15d67efa1f05f14d68f1
Onn Khairuddin bin Rahmat <onn.khairuddin@gmail.com>
2018-04-02 16:27:46 +0800

    Initial import chapter 4.

---
+++ chapter4/Groceries/app/views/login/login.js
@@ -0,0 +1,23 @@
+var frameModule = require("ui/frame");
+var page;
+var email;
+
+var observableModule = require("data/observable");
+var user = new observableModule.fromObject({
+	email: "user@domain.com",
+	password: "password"
+});
+
+exports.loaded = function(args) {
+	page = args.object;
+	page.bindingContext = user;
+};
+exports.signIn = function() {
+	email = page.getViewById("email");
+	console.log(email.text);
+};
+exports.register = function() {
+	var topmost = frameModule.topmost();
+	topmost.navigate("views/register/register");
+};
+
