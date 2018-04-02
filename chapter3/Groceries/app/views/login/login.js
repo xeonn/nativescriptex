71d33b659d5669c48e8eb1d0ff061b01b1433657
Onn Khairuddin bin Rahmat <onn.khairuddin@gmail.com>
2018-04-02 16:15:27 +0800

    Navigating screen.

--- chapter3/Groceries/app/views/login/login.js
+++ chapter3/Groceries/app/views/login/login.js
@@ -1,10 +1,23 @@
-exports.loaded = function() {
-	console.log("hello");
-};
+var observable = require("data/observable");
+var frameModule = require("ui/frame");
+var page;
+var email;
+var password;
+var user = new observableModule.fromObject({
+	email: "user@domain.com",
+	password: "password"
+});
 
+exports.loaded = function(args) {
+	page = args.object;
+	page.bindingContext = user;
+};
 exports.signIn = function() {
-	alert("Signing in");
+	email = page.getViewById("email");
+	console.log(email.text);
 };
 exports.register = function() {
-	alert("Registering");
+	var topmost = frameModule.topmost();
+	topmost.navigate("views/register/register");
 };
+
